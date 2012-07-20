9a9c68c95ffc5b8217e6e03a04334f092d2b750d
Al <alastair@propcom.co.uk>
2012-07-20 12:17:34 +0100

    Make it a package with a module in it

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+Config::set('module_paths', Config::get('module_paths') + [ __DIR__ . '/modules/' ]);
+Autoloader::add_classes([
+	'Datagenerator\DataGenerator' => __DIR__.'/classes/datagenerator.php',
+	'Datagenerator\FieldTemplate' => __DIR__.'/classes/datagenerator.php',
+]);
