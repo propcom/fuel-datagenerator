c54a7d7c2e2689912f3dcb612ec5353dd4cb61a7
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-01-09 12:45:51 +0000

    Wrong path for FieldTemplate class file

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,5 @@
 Config::set('module_paths', Config::get('module_paths') + [ __DIR__ . '/modules/' ]);
 Autoloader::add_classes([
 	'Datagenerator\DataGenerator' => __DIR__.'/classes/datagenerator.php',
-	'Datagenerator\FieldTemplate' => __DIR__.'/classes/datagenerator.php',
+	'Datagenerator\FieldTemplate' => __DIR__.'/classes/fieldtemplate.php',
 ]);
