f1133eaff53a7eef7c83e9441753aea0ab8aaf60
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-01-09 12:53:51 +0000

    Three-line version of module path addition

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
-Config::set('module_paths', Config::get('module_paths') + [ __DIR__ . '/modules/' ]);
+$module_paths = \Config::get('module_paths');
+array_push($module_paths,  __DIR__ . '/modules/' );
+\Config::set('module_paths', $module_paths);
+
 Autoloader::add_classes([
 	'Datagenerator\DataGenerator' => __DIR__.'/classes/datagenerator.php',
 	'Datagenerator\FieldTemplate' => __DIR__.'/classes/fieldtemplate.php',
